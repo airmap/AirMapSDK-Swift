1057e30b3a91c1bd6e34bf1b3eb61aa9c2c87663
Rocky Demoff <rocky@airmap.com>
2016-10-17 17:34:43 -0700

    Swizzling Alert Title

--- Source/UI/View Controllers/AirMapAuthViewController.swift
+++ Source/UI/View Controllers/AirMapAuthViewController.swift
@@ -149,5 +149,43 @@ extension AFJSONResponseSerializer {
 	
 }
 
+extension UIAlertController {
+	
+	public override class func initialize() {
+		
+		guard self == NSClassFromString("UIAlertController") else {
+			return
+		}
+		
+		struct Static {
+			static var token: dispatch_once_t = 0
+		}
+		
+		dispatch_once(&Static.token) {
+			let originalSelector = Selector("viewWillAppear:")
+			let swizzledSelector = Selector("airmap_viewWillAppear:")
+			
+			let originalMethod = class_getInstanceMethod(self, originalSelector)
+			let swizzledMethod = class_getInstanceMethod(self, swizzledSelector)
+			
+			let didAddMethod = class_addMethod(self, originalSelector, method_getImplementation(swizzledMethod), method_getTypeEncoding(swizzledMethod))
+			
+			if didAddMethod {
+				class_replaceMethod(self, swizzledSelector, method_getImplementation(originalMethod), method_getTypeEncoding(originalMethod))
+			} else {
+				method_exchangeImplementations(originalMethod, swizzledMethod)
+			}
+		}
+	}
+	
+	
+	
+	public func airmap_viewWillAppear(animated: Bool) {
+		if self.title == "There was an error logging in" || self.title == "There was an error signing up" {
+			self.title = "Alert"
+		}
+	}
+}
+
 
 
