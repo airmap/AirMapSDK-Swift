a415bded23efa0fdd9f053923d9c54ede6f91988
Michael Odere <Michael@AirMap.com>
2020-01-28 13:06:05 -0800

    SystemStatus.Level

--- Source/SystemStatus/SystemStatus.swift
+++ Source/SystemStatus/SystemStatus.swift
@@ -7,28 +7,26 @@
 
 import ObjectMapper
 
-public enum SystemStatusLevel: String {
-	case normal = "LEVEL_NORMAL"
-	case downgraded = "LEVEL_DEGRADED"
-	case unrecognized
-}
-
 public struct SystemStatus {
-	public let level: SystemStatusLevel
+	public let level: SystemStatus.Level
 	public let message: String
-	public let generatedAt: Date
-	public let submittedAt: Date
+
+	public enum Level: String {
+		case unkown
+		case normal
+		case maintenance
+		case degraded
+		case failed
+	}
+
 }
 
 
 extension SystemStatus: ImmutableMappable {
-
 	public init(map: Map) throws {
 		do {
 			level       = try map.value("level", using: SystemStatusLevelTransform())
 			message     =  try map.value("message")
-			generatedAt =  try map.value("generated_at", using: DateTransform())
-			submittedAt =  try map.value("submitted_at", using: DateTransform())
 		}
 		catch {
 			AirMap.logger.error("Failed to parse SystemStatus", metadata: ["error": .string(error.localizedDescription)])
@@ -39,17 +37,17 @@ extension SystemStatus: ImmutableMappable {
 
 class SystemStatusLevelTransform: TransformType {
 
-	typealias Object = SystemStatusLevel
+	typealias Object = SystemStatus.Level
 	typealias JSON = String
 
-	func transformFromJSON(_ value: Any?) -> SystemStatusLevel? {
+	func transformFromJSON(_ value: Any?) -> SystemStatus.Level? {
 		if let string = value as? String {
-			return SystemStatusLevel(rawValue: string)
+			return SystemStatus.Level(rawValue: string)
 		}
 		return nil
 	}
 
-	func transformToJSON(_ value: SystemStatusLevel?) -> String? {
+	func transformToJSON(_ value: SystemStatus.Level?) -> String? {
 		if let level = value {
 			return level.rawValue
 		}
